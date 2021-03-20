c9a0c62d7e240c46f0f6040cc3d2a7c4b42e5784
liuxiaofeng <120851062@qq.com>
2021-03-20 22:36:20 +0800

    '主页开发完成'

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,72 @@
 <template>
-    <div>
-        <el-button type="info" @click="logout">退出</el-button>
-    </div>
+
+<el-container class="home-container">
+  <!-- 头部 -->
+  <el-header>
+      <div>
+          <!-- <img src="" alt=""> -->
+          <span>电商后台管理系统</span>
+      </div>
+      <el-button type="info" @click="logout">退出</el-button>
+  </el-header>
+  <el-container>
+    <!-- 侧边栏 -->
+    <el-aside :width="isCollapse ? '64px' :'200px'">
+      <div class="toggle-button" @click="toggleCollapse">|||</div>
+        <!-- 侧边栏菜单效果 -->
+        <el-menu
+      background-color="#333744"
+      text-color="#fff"
+      :unique-opened="true"
+      active-text-color="#409EFF" 
+      :collapse="isCollapse"
+      :collapse-transition="false"
+      :router="true">
+      <!-- 一级菜单 -->
+      <el-submenu :index="item.id + ''" v-for="item in menuList" :key="item.id">
+        <!-- 一级菜单的模板区域 -->
+        <template slot="title">
+          <!-- 图标 -->
+          <i :class="iconsObj[item.id]"></i>
+          <!-- 文本 -->
+          <span>{{item.authName}}</span>
+        </template>
+        <!-- 二级菜单 -->
+        <el-menu-item :index="'/'+subItem.path  " v-for="subItem in item.children" :key="subItem.id">
+          <template slot="title">
+          <!-- 图标 -->
+          <i class="el-icon-menu"></i>
+          <span>{{subItem.authName}}</span>
+        </template>
+        </el-menu-item>
+      </el-submenu>
+      
+    </el-menu>
+    </el-aside>
+    <!-- 主体区域 -->
+    <el-main>
+      <router-view></router-view>
+    </el-main>
+  </el-container>
+</el-container>
 </template>
 <script>
 export default {
+  created(){
+    this.getMenuList()
+  },
     data(){
         return{
+          menuList:[],
+          iconsObj: {
+        '125': 'iconfont icon-user',
+        '103': 'iconfont icon-tijikongjian',
+        '101': 'iconfont icon-shangpin',
+        '102': 'iconfont icon-danju',
+        '145': 'iconfont icon-baobiao'
+      },
+      //是否折叠
+      isCollapse:false,
 
         }
     },
@@ -16,10 +76,56 @@ export default {
             this.$router.push('/login')
             this.$message('退出成功');
 
+        },
+        // 获取所有的菜单
+        async getMenuList(){
+          //结构赋值
+          const {data : res} =await this.$http.get('menus')
+          if(res.meta.status !==200){
+            return this.$message.error(res.meta.msg)
+          }
+          this.menuList =res.data
+          console.log(res);
+        },
+        toggleCollapse(){
+          this.isCollapse = !this.isCollapse;
+
         }
     }
 }
 </script>
 <style lang="less" scoped>
-
+.home-container{
+    height: 100%;
+}
+.el-header{
+    display: flex;
+    justify-content: space-between;
+    padding-left: 15px;
+    background-color: #373d41;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+}
+.el-aside{
+    background-color: #333744;
+    .el-menu {
+      border-right:none
+    }
+}
+.el-main{
+    background-color: #eaedf1;
+}
+.iconfont{
+  margin-right:10px ;
+}
+.toggle-button{
+  background-color: #4A5064;
+  font-size: 10px;
+  line-height: 24px;
+  color: #fff;
+  text-align: center;
+  letter-spacing: 0.2em;
+  cursor: pointer;
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -3,12 +3,19 @@ import App from './App.vue'
 import router from './router'
 import './plugins/element.js'
 import './assets/css/global.css'
+import './assets/fonts/iconfont.css'
 // 导入axios
 import axios from 'axios'
 // 讲axios挂载到vue的原型对象上,这样vue的组件都可以利用this.$http访问axios请求
 Vue.prototype.$http = axios
     // 为axios设置请求根路径
 axios.defaults.baseURL = 'http://127.0.0.1:8888/api/private/v1/'
+axios.interceptors.request.use(config => {
+    // console.log(config);
+    config.headers.Authorization = window.sessionStorage.getItem('token')
+        // 必须return config
+    return config
+})
 Vue.config.productionTip = false
 
 new Vue({
